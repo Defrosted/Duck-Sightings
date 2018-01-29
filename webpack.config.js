c874c1c1f1a175179382d44d8b9a1a666ae2d850
Defrosted <ermurj@hotmail.com>
2018-01-29 03:17:33 +0200

    Added LESS compilation to build and dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,15 @@
 const HtmlPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const extractLess = new ExtractTextPlugin( {
+    filename: '[name].css'
+});
 
 module.exports = {
-    entry: __dirname + '/src/static/js/App.js',
+    entry: [
+        __dirname + '/src/static/js/App.js',
+        __dirname + '/src/static/css/style.less'
+    ],
 
     output: {
         filename: 'App.js',
@@ -11,16 +19,29 @@ module.exports = {
     devtool: 'source-map',
 
     module: {
-        loaders: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }]
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                use: extractLess.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "less-loader"
+                    }],
+                })
+            }
+        ]
     },
 
     plugins: [
         new HtmlPlugin({
             template: 'src/static/index.html'
-        })
+        }),
+        extractLess
     ]
 }
